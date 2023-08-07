4a4f11513131589b579a3c7493339e08cf725f22
Derek Prieur <DPrieur123@gmail.com>
2023-08-07 12:38:18 -0400

    first

---
+++ lib/validations/thread.ts
@@ -0,0 +1,10 @@
+import * as z from 'zod';
+
+export const threadValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: 'Thread must be at least 3 characters long' }),
+    accountId: z.string()
+});
+
+export const commentValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: 'Thread must be at least 3 characters long' }),
+});
